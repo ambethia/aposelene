d127471873b6a593d8db6e68d965f09cd1b7c7d2
Jason L Perry <jasper@ambethia.com>
2012-02-06 03:49:46 -0500

    noise

--- post.f.glsl
+++ post.f.glsl
@@ -1,6 +1,15 @@
 uniform sampler2D fbo_texture;
 varying vec2 f_texcoord;
- 
+
+float rand(vec2 co){
+  return fract(sin(dot(co.xy ,vec2(12.9898,78.233))) * 43758.5453);
+}
+
 void main(void) {
-  gl_FragColor = texture2D(fbo_texture, f_texcoord);
+  float gray = rand(f_texcoord);
+  gl_FragColor = mix(
+    texture2D(fbo_texture, f_texcoord),
+    vec4(gray,gray,gray,0.5),
+    0.1
+  );
 }
